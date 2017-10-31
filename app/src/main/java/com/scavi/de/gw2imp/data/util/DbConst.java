b680f12298a884d1701bdbc640acf780e7dfda92
Michael Heymel <cfa.scavenger@gmx.de>
2017-10-31 19:42:43 +0100

    Initial version

---
+++ app/src/main/java/com/scavi/de/gw2imp/data/util/DbConst.java
@@ -0,0 +1,26 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.scavi.de.gw2imp.data.util;
+
+public class DbConst {
+    public static final String TABLE_RAID = "Raid";
+    public static final String TABLE_ACHIEVEMENT = "Achievement";
+    public static final String TABLE_ACHIEVEMENT_FLAGS = "AchievementFlags";
+    public static final String TABLE_ACHIEVEMENT_REWARDS = "AchievementRewards";
+    public static final String TABLE_ACHIEVEMENT_TIERS = "AchievementTiers";
+
+    private DbConst() {
+
+    }
+}
