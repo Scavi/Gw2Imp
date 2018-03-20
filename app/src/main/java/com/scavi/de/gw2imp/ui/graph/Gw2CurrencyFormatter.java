75e4d09dcb3d10ae12b76af25b49dc3b3b0cb8d2
Michael Heymel <cfa.scavenger@gmx.de>
2018-03-20 21:44:24 +0100

    changed the labels to mm/yy

--- app/src/main/java/com/scavi/de/gw2imp/ui/graph/Gw2CurrencyFormatter.java
+++ app/src/main/java/com/scavi/de/gw2imp/ui/graph/Gw2CurrencyFormatter.java
@@ -6,36 +6,80 @@ import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.scavi.de.gw2imp.R;
 
 import java.text.DecimalFormat;
+import java.util.Calendar;
 
 public class Gw2CurrencyFormatter extends DefaultLabelFormatter {
     private enum MoneyType {G, S, K}
+
+    public static final int ONE_GRID_LENGTH = 19;
+    public static final int HORIZONTAL_MAX_VIEW = 50;
 
     private final int SILVER_LIMIT = 100;
     private final int GOLD_LIMIT = SILVER_LIMIT * 100;
     private final Context mContext;
     private final DecimalFormat mFormat = new DecimalFormat();
+    private final int mHistoryDataSetCount;
 
-    public Gw2CurrencyFormatter(final Context context) {
+    /**
+     * Constructor
+     *
+     * @param context             the context to global information about the application
+     *                            environment
+     * @param historyDataSetCount the amount of history entries
+     */
+    public Gw2CurrencyFormatter(final Context context,
+                                final int historyDataSetCount) {
         mContext = context;
+        mHistoryDataSetCount = historyDataSetCount;
         mFormat.setMaximumFractionDigits(2);
     }
 
     @Override
-    public String formatLabel(double value,
+    public String formatLabel(final double value,
                               final boolean isValueX) {
-        if (isValueX) {
-            // show normal x values
-            return null;
+        return isValueX ? createHorizontalLabel(value) :
+                createVerticalLabel(value);
+    }
+
+
+    /**
+     * Creates the horizontal label for history prices (since history prices are in the grid).
+     * For current prices the month / dates won't be determined because the labels would overlap.
+     *
+     * @param value 0, 20, 40, ...
+     * @return the horizontal label
+     */
+    private String createHorizontalLabel(final double value) {
+        int step = (int) (value / (ONE_GRID_LENGTH + 1));
+        int monthDifference = step - mHistoryDataSetCount;
+        if (monthDifference <= 0) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.MONTH, monthDifference);
+            String date = String.format("%s/%s",
+                    calendar.get(Calendar.MONTH) + 1,
+                    String.valueOf(calendar.get(Calendar.YEAR)).substring(2));
+            return date;
         } else {
-            MoneyType type = typeFrom(value);
-            value = createCurrencyString(type, value);
-            // show currency for y values
-            return String.format(mContext.getString(R.string.trading_items_currency_format),
-                    mFormat.format(value),
-                    type);
+            return null;
         }
     }
 
+
+    /**
+     * Creates the vertical label based on the price. Recalculates the given copper depending on the
+     * currency.
+     *
+     * @param value the value
+     * @return the vertical label
+     */
+    private String createVerticalLabel(double value) {
+        MoneyType type = typeFrom(value);
+        value = createCurrencyString(type, value);
+        // show currency for y values
+        return String.format(mContext.getString(R.string.trading_items_currency_format),
+                mFormat.format(value),
+                type);
+    }
 
     /**
      * Since all money values are specified by copper, this method will calculate the
