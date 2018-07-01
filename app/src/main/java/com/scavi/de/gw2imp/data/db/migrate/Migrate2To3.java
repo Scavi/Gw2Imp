d55cd119920c43fa14df87d4af8c7d0c608e2ff8
Michael Heymel <cfa.scavenger@gmx.de>
2018-07-01 16:48:49 +0200

    Added the strategy from migrate from version 2 to 3

---
+++ app/src/main/java/com/scavi/de/gw2imp/data/db/migrate/Migrate2To3.java
@@ -0,0 +1,37 @@
+package com.scavi.de.gw2imp.data.db.migrate;
+
+import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.room.migration.Migration;
+import android.support.annotation.NonNull;
+
+import com.scavi.de.gw2imp.data.entity.item.ItemSearchEntity;
+import com.scavi.de.gw2imp.data.entity.item.TrendEntity;
+import com.scavi.de.gw2imp.data.util.DbConst;
+
+/**
+ * Migrates from version 2 to version 3 of the database. The main changes are:
+ * - Add the table Trend {@link TrendEntity} which allows to register trend items (e.g. seller /
+ * buyer prices)
+ * - Add the table ItemSearch {@link ItemSearchEntity} which optimizes the item search
+ */
+public class Migrate2To3 extends Migration {
+
+    /**
+     * Constructor
+     * <p>
+     * Migrate from 2 to 3
+     */
+    public Migrate2To3() {
+        super(2, 3);
+    }
+
+    @Override
+    public void migrate(@NonNull final SupportSQLiteDatabase database) {
+        // creates the table item search part
+        database.execSQL("CREATE TABLE IF NOT EXISTS " + DbConst.TABLE_ITEM_PART_SEARCH + " (id " +
+                "INTEGER, namePart TEXT, PRIMARY KEY(id, namePart))");
+        // creates the table trends
+        database.execSQL("CREATE TABLE IF NOT EXISTS " + DbConst.TABLE_TRENDS + " (id INTEGER, " +
+                "type INTEGER, percentage DOUBLE, time LONG, PRIMARY KEY(id, type))");
+    }
+}
